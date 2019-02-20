3ccabd260d9c90749b9c95c95cb0692de5320ca8
VJ <VJ@216.37.96.215>
2019-02-19 20:38:13 -0500

    Final Version

--- src/feb19/LatinSquare.java
+++ src/feb19/LatinSquare.java
@@ -11,34 +11,67 @@ public class LatinSquare {
 	}
 
 	public static void main(String[] args) {
-		/*
-		 * int[][] arr2D = { { 10, 30, 20, 0 }, { 0, 20, 30, 10 }, { 30, 0, 10, 20 }, {
-		 * 20, 10, 0, 30 } };
-		 * 
-		 * int[][] arr2D = { { 1, 2, 3 }, { 3, 1, 2 }, { 2, 3, 1 }, };
-		 * 
-		 * int[][] arr2D = { { 1, 2, 3 }, { 3, 1, 2 }, { 7, 8, 9 }, };
-		 * 
-		 * 
-		 * int[][] arr2D = { { 10, 30, 20, 0 }, { 20, 20, 30, 10 }, { 30, 0, 10, 20 }, {
-		 * 20, 10, 0, 30 } };
-		 */
-		int[][] arr2D = { { 10, 30, 20, 0 }, { 0, 20, 30, 10 }, { 30, 0, 10, 16 }, { 20, 10, 0, 30 } };
+		
+		  int[][] arr2D1 = { { 10, 30, 20, 0 }, 
+		  					{ 0, 20, 30, 10 }, 
+		  					{ 30, 0, 10, 20 }, 
+		  					{20, 10, 0, 30 } };
+		  
+		  int[][] arr2D2 = { { 1, 2, 3 }, 
+		  					{ 3, 1, 2 }, 
+		  					{ 2, 3, 1 }, };
+		  
+		  int[][] arr2D3 = { { 1, 2, 3 }, 
+				  			{ 3, 1, 2 }, 
+				  			{ 7, 8, 9 }, };
+		  
+		  int[][] arr2D4 = { { 10, 30, 20, 0 }, 
+				            { 20, 20, 30, 10 }, 
+				            { 30, 0, 10, 20 }, 
+				            {20, 10, 0, 30 } };
+		 
+		 int[][] arr2D5 = { { 10, 30, 20, 0 }, 
+				            { 0, 20, 30, 10 }, 
+				            { 30, 0, 10, 16 }, 
+				            { 20, 10, 0, 30 } };
 
-		printArray2D(arr2D);
-
-		if (isLatin(arr2D))
-			System.out.println("\nYes, 2D array is a Latin Square");
+		printArray2D(arr2D1);
+		if (isLatin(arr2D1))
+			System.out.println("Yes, 2D array is a Latin Square");
 		else
-			System.out.println("\nNO, 2D array is NOT a Latin Square");
+			System.out.println("NO, 2D array is NOT a Latin Square");
+		
+		printArray2D(arr2D2);
+		if (isLatin(arr2D2))
+			System.out.println("Yes, 2D array is a Latin Square");
+		else
+			System.out.println("NO, 2D array is NOT a Latin Square");
+		
+		printArray2D(arr2D3);
+		if (isLatin(arr2D3))
+			System.out.println("Yes, 2D array is a Latin Square");
+		else
+			System.out.println("NO, 2D array is NOT a Latin Square");
+		
+		printArray2D(arr2D4);
+		if (isLatin(arr2D4))
+			System.out.println("Yes, 2D array is a Latin Square");
+		else
+			System.out.println("NO, 2D array is NOT a Latin Square");
+		
+		printArray2D(arr2D5);
+		if (isLatin(arr2D5))
+			System.out.println("Yes, 2D array is a Latin Square");
+		else
+			System.out.println("NO, 2D array is NOT a Latin Square");
 
 	}
 
 	public static boolean isLatin(int[][] arr2d) {
-//System.out.println("1)CREATE TEMP ARRAY AND STRING");
 		// create place to store elements for comparison --> ARRAY IDEA NOT USED
 		numElements = arr2d[0].length;
 		int[] storedVals = new int[numElements];
+
 		// String to store values
 		String val = "";
 
@@ -46,75 +79,56 @@ public class LatinSquare {
 			storedVals[i] = arr2d[0][i];
 			val += " " + arr2d[0][i] + ",";
 		}
-//System.out.print("\n VAl String = " + val);
-//System.out.print("\n VAL INDEX TEST = " + val.indexOf(" " + 0 + ""));
-		// call duplicate
 
-		// rows
-//System.out.println("\n2)CHECK ROW");
+		// Check rows
 		for (int i = arr2d.length - 1; i >= 0; i--) {
-//System.out.print("jfkjyfkyhjgjgyhgygygygh");
 			boolean t = duplicate(arr2d[i]);
-//System.out.println(t);
-//System.out.println(i);
 			if (t) {
-				System.out.println("\nRow " + i + " has duplicates.");
+				System.out.println("Row " + i + " has duplicates.");
 				return false;
 			} else {
-				for (int q = 0; q < arr2d[i].length; q++) { // check against stored values
-//System.out.println("INDEEXESSS" + val.indexOf(" " + arr2d[i][q]));
-//System.out.println(arr2d[i][q]);
+				// check against stored values
+				for (int q = 0; q < arr2d[i].length; q++) {
 					if (val.indexOf(" " + arr2d[i][q]) == -1) {
-						System.out.println("\nRow " + i + " does not have all row 0 elements");
+						System.out.println("Row " + i + " does not have all row 0 elements");
 						return false;
 					}
 				}
 			}
-
 		}
 
-		// columns
-//System.out.println("3)CHECK COLUMN");
-		// make array
+		// Check columns
+		// make temp array for comparison
 		int[] temp = new int[arr2d.length];
+
 		for (int i = arr2d.length - 1; i >= 0; i--) {
 			for (int j = 0; j < arr2d.length; j++) {
-//System.out.println("		COLUMNCICLDKCMSDKJCNKDSJCNJDSCNJDSBCJHDSCHJDSCHJDSHCJDBSHC");
 				temp[j] = arr2d[j][i];
-//System.out.println("(" + j + "),(" + i + "): " + temp[j]);
 			}
+			// call duplicate
 			boolean t = duplicate(temp);
 			if (t) {
-				System.out.println("\nColumn " + i + " has duplicates.");
+				System.out.println("Column " + i + " has duplicates.");
 				return false;
 			} else {
-				for (int q = 0; q < temp.length; q++) { // check against stored values
+				// check against stored values
+				for (int q = 0; q < temp.length; q++) {
 					if (val.indexOf(" " + temp[q]) == -1) {
-						System.out.println("\nColumn " + i + " does not have all row 0 elements");
+						System.out.println("Column " + i + " does not have all row 0 elements");
 						return false;
 					}
 				}
 			}
-
 		}
 
-		/*
-		 * is true if: - square -no duplicates in each row/column -
-		 */
-
-		// duplicate
-		// duplicate(arr2d[0]);
 		return true;
 	}
 
 	public static boolean duplicate(int[] arr) {
-		// 1) check duplicate, if false, then store the values in array.
-//System.out.println("\n");
 		for (int i = 0; i < arr.length - 1; i++) {
 			for (int j = i + 1; j < arr.length; j++) {
-//System.out.println(arr[i] + " compared with " + arr[j]);
 				if (arr[i] == arr[j]) {
-
+					// if values are same:
 					return true;
 				}
 			}
@@ -136,6 +150,6 @@ public class LatinSquare {
 					System.out.print(arr2d[i][q] + " ");
 				}
 			}
-		}
+		}System.out.print("\n");
 	}
 }
