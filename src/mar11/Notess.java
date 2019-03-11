090394fe9126d177226190603a299eccd0047087
VJ <VJ@216.37.98.241>
2019-03-11 19:33:23 -0400

    Notes of the day

---
+++ src/mar11/Notess.java
@@ -0,0 +1,26 @@
+package mar11;
+
+public class Notess {
+
+	public Notess() {
+		// TODO Auto-generated constructor stub
+		
+		/*
+		 * Overload: methods with same name, but diff parameters in the same class
+		 * Override: change method contents of method in base class
+		 */
+		
+		
+		/*
+		 * Abstrct classes:
+		 * 	CANNOT be instantiated ; cannot be called or create an object of it
+		 * 	Serves as a superclass
+		 * 	MUST HAVE abstract method 
+		 * 		must be overriden in subclass
+		 * 		no body, just header and then a semicolon ;
+		 */
+		
+	}
+	
+
+}
